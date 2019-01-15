6b926c111f490e38beae255c97ff4b2826b1039b
Colin White <clwhite814@gmail.com>
2019-01-15 09:45:53 -0600

    displaying current weather from API

--- src/components/pages/WeatherItem/WeatherItem.js
+++ src/components/pages/WeatherItem/WeatherItem.js
@@ -1,26 +1,6 @@
 import React from 'react';
-import { Label, Input } from 'reactstrap';
-import weatherRequests from '../../../helpers/data/weatherRequests';
 
 class WeatherItem extends React.Component {
-  state = {
-
-  }
-
-  updateFirebase = (e) => {
-    e.preventDefault();
-    const weatherId = this.props.weather.id;
-    const isTheCurrent = this.props.weather.isCurrent;
-    weatherRequests.patchIsCurrent(weatherId, isTheCurrent)
-      .then(() => {
-        console.log(this.props.weather.isCurrent);
-        weatherRequests.getWeather(this.props.uid);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   render() {
     const { weather } = this.props;
     return (
@@ -28,9 +8,6 @@ class WeatherItem extends React.Component {
         <li className="weather-items">
           <span>{weather.city}, </span>
           <span>{weather.state}</span>
-      <Label check>
-            <Input type="radio" onChange={this.updateFirebase} />
-      </Label>
         </li>
       </div>
     );
